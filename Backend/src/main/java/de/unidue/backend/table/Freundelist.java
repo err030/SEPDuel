72134b7d0b0c7710656a4995a6e78021d0365730
Zheyuan_Wu <zheyuan.wu@stud.uni-due.de>
2024-04-23 13:49:24 +0200

    freundelist table

---
+++ Backend/src/main/java/de/unidue/backend/table/Freundelist.java
@@ -0,0 +1,14 @@
+package de.unidue.backend.table;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Freundelist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Long id;
+
+}
