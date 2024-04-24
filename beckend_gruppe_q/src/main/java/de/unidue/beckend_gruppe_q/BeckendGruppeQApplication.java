50caa76b6a2fd50ba93c8935f5fd26f684c4ec81
Yuchen Teng <yuchen.teng@stud.uni-due.de>
2024-04-24 14:15:58 +0200

    backend mit mysql

---
+++ beckend_gruppe_q/src/main/java/de/unidue/beckend_gruppe_q/BeckendGruppeQApplication.java
@@ -0,0 +1,13 @@
+package de.unidue.beckend_gruppe_q;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeckendGruppeQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeckendGruppeQApplication.class, args);
+    }
+
+}
