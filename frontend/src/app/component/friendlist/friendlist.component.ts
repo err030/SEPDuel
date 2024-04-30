2af9f0c0fd59035de7814da2aad98b88c1048507
Zheyuan_Wu <zheyuan.wu@stud.uni-due.de>
2024-04-30 13:39:33 +0200

    friendlist component js html

---
+++ frontend/src/app/component/friendlist/friendlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friendlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './friendlist.component.html',
+  styleUrl: './friendlist.component.css'
+})
+export class FriendlistComponent {
+
+}
