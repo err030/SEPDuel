0eef53938d4852d51b21f6ee2d71f51d07870cca
Beibei Tu <beibei.tu@stud.uni-due.de>
2024-05-09 11:51:01 +0200

    docker

---
+++ beckend_gruppe_q/src/main/java/de/unidue/beckend_gruppe_q/service/SpecialCardTypeService.java
@@ -0,0 +1,34 @@
+package de.unidue.beckend_gruppe_q.service;
+
+
+import de.unidue.beckend_gruppe_q.model.Card;
+import de.unidue.beckend_gruppe_q.model.Rarity;
+import de.unidue.beckend_gruppe_q.repository.CardRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpecialCardTypeService {
+
+    private final CardRepository cardRepository;
+
+    public SpecialCardTypeService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+    @PostConstruct
+    public void initSpecialCardType() {
+        if (cardRepository.findByCardName("O Deus Klaus")==null) {
+            Card specialCardType = new Card();
+
+            specialCardType.setCardName("O Deus Klaus");
+            specialCardType.setCardRarity(Rarity.LEGENDARY);
+            specialCardType.setAttackPoints(Integer.MAX_VALUE);
+            specialCardType.setDefensePoints(0);
+            specialCardType.setDescription("");
+            specialCardType.setImage("");
+
+            this.cardRepository.save(specialCardType);
+        }
+    }
+}
