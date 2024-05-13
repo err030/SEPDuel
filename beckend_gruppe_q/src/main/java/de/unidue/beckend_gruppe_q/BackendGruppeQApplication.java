d78c3555e212461297faaf44fcf5ac931b4fdfb2
Zijie Liu <zijie.liu@outlook.com>
2024-05-13 18:52:59 +0200

    test cases

--- beckend_gruppe_q/src/main/java/de/unidue/beckend_gruppe_q/BackendGruppeQApplication.java
+++ beckend_gruppe_q/src/main/java/de/unidue/beckend_gruppe_q/BackendGruppeQApplication.java
@@ -7,23 +7,28 @@ import de.unidue.beckend_gruppe_q.model.User;
 import de.unidue.beckend_gruppe_q.repository.CardRepository;
 import de.unidue.beckend_gruppe_q.repository.DeckRepository;
 import de.unidue.beckend_gruppe_q.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.hibernate.Hibernate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@Transactional
 public class BackendGruppeQApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendGruppeQApplication.class, args);
     }
-    /*@Bean
+
+    @Bean
     public CommandLineRunner demo(DeckRepository deckRepository, CardRepository cardRepository, UserRepository userRepository) {
-        return args -> {*/
-//            // create a deck
+        return args -> {
+//            create a deck
 //            Deck deck = new Deck();
 //            deck.setName("Deck");
 //            deckRepository.save(deck);
@@ -38,15 +43,33 @@ public class BackendGruppeQApplication {
 //            user.cards.add(card2);
 //
 //            userRepository.save(user);
-//
-          /*  for (User u : userRepository.findAll()) {
-                u.cards.add(new Card("test", Rarity.COMMON, 1, 1, "A card for testing", ""));
+            for (User u : userRepository.findAll()) {
+                u.cards.clear();
+                u.decks.clear();
                 userRepository.save(u);
             }
+//
+            for (User u : userRepository.findAll()) {
+                u.cards.add(new Card("test", Rarity.COMMON, 1, 1, "A card for testing", ""));
+                u.cards.add(new Card("桃园结义", Rarity.COMMON, 1, 1, "A card for testing", ""));
+                u.cards.add(new Card("顺手牵羊", Rarity.COMMON, 1, 1, "A card for testing", ""));
+                u.cards.add(new Card("无懈可击", Rarity.COMMON, 1, 1, "A card for testing", ""));
+                u.cards.add(new Card("铁索连环", Rarity.COMMON, 1, 1, "A card for testing", ""));
+                userRepository.save(u);
+            }
+            User u = userRepository.findById(1L).get();
 
-        };*/
- //   }
+            List<Deck> d = u.decks;
+            for (int i = 1; i < 4; i++) {
+                List<Card> c = new ArrayList<>();
+                d.add(new Deck("Deck " + i, "", c));
 
+                c.add(new Card("test", Rarity.COMMON, 1, 1, "A card for testing", ""));
+            }
+            userRepository.save(u);
+        };
+    }
 }
+
 
 
