32e2347d530b285fd5862b9ec3a238d1c5cabc57
Yuchen Teng <yuchen.teng@stud.uni-due.de>
2024-05-16 23:04:08 +0200

    admin see all friendlist

--- frontend/src/app/component/admin-userlist/admin-userlist.component.ts
+++ frontend/src/app/component/admin-userlist/admin-userlist.component.ts
@@ -2,16 +2,23 @@ import {Component, OnInit} from '@angular/core';
 import {User} from "../../model/user";
 import {UserService} from "../../service/user.service";
 import {ConfirmationService, MessageService} from "primeng/api";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router, RouterOutlet} from "@angular/router";
 import {FriendService} from "../../service/friend.service";
 import {Global} from "../../global";
 import {TableModule} from "primeng/table";
+import {friend} from "../../model/friend";
+import {NgClass, NgForOf} from "@angular/common";
+import {DialogModule} from "primeng/dialog";
 
 @Component({
   selector: 'app-admin-userlist',
   standalone: true,
   imports: [
-    TableModule
+    TableModule,
+    NgClass,
+    DialogModule,
+    NgForOf,
+    RouterOutlet
   ],
   providers: [UserService, FriendService,MessageService,ConfirmationService],
   templateUrl: './admin-userlist.component.html',
@@ -23,8 +30,14 @@ export class AdminUserlistComponent implements OnInit{
   public loggedUser: any;
   allComment:Comment [] =[];
   allUser:User[]=[];
+  selectedUserListItemId: number | undefined;
+  friendId: number | null = null;
+  selectedUser: User | null = null;
+  showFriendList: boolean = false;
+  friends: User[] = [];
 
-  constructor(private userService: UserService,
+  constructor(private activatedRoute: ActivatedRoute,
+              private userService: UserService,
               private messageService: MessageService,
               private friendService: FriendService,
               private router: Router) {
@@ -33,6 +46,15 @@ export class AdminUserlistComponent implements OnInit{
   ngOnInit(): void {
     this.loggedUser = Global.loggedUser;
     this.getAllUsers();
+    this.activatedRoute.paramMap.subscribe(parameters => {
+      const friendIdParam = parameters.get('friendId');
+      if (friendIdParam != null) {
+        this.friendId = parseInt(friendIdParam);
+        this.loggedUser = Global.loggedUser;
+        this.selectedUser = this.friendService.selectedFriend;
+
+      }
+    })
   }
 
   getAllUsers(): void {
@@ -48,5 +70,41 @@ export class AdminUserlistComponent implements OnInit{
     );
   }
 
+  getItemClass(user: User): string {
+    if (user.id == this.selectedUserListItemId) {
+      return 'userListItemSelected';
+    } else {
+      return 'userListItemUnselected';
+    }
+  }
 
+  openFriendlistDialog(user: User): void {
+    this.showFriendList = true;
+    this.selectedUserListItemId = user.id;
+    this.friendService.selectedFriend = user;
+    if (this.selectedUser && this.selectedUser.id) {
+      this.friendService.getAllFriends(this.selectedUser.id).subscribe({
+        next: (response) => {
+          if (response.status == 200 && response.body) {
+            this.friends.length = 0;
+            response.body.forEach(friend => {
+              this.friends.push(friend);
+            })
+            this.showFriendList = true;
+            console.log(response.body);
+          }
+        },
+        error: (error) => {
+          this.messageService.add({severity: 'error', summary: 'Fehler', detail: error.statusText});
+        }
+      })
+    }
+  }
+
+  onUserListItemClick(user: User): void {
+    this.selectedUserListItemId = user.id;
+    this.friendService.selectedFriend = user;
+    void this.router.navigateByUrl("/admin-userlist/allfriendlist/" + user.id);
+
+  }
 }
