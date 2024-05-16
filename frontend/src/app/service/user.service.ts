a9aa7576936d882fa88ae2ebf5c392da51581d97
Yuchen Teng <yuchen.teng@stud.uni-due.de>
2024-05-16 16:01:30 +0200

    admin

--- frontend/src/app/service/user.service.ts
+++ frontend/src/app/service/user.service.ts
@@ -172,7 +172,7 @@ export class UserService {
     }
   }
 
-  getAllUser(user: User): Observable<HttpResponse<User[]>> {
+  getAllUserByGroupId(user: User): Observable<HttpResponse<User[]>> {
     const url = Global.userRestServiceUrl + "/getAllUser "+ "/" + user.groupId;
     return this.http.get<User[]>(url, {observe: 'response'});
   }
