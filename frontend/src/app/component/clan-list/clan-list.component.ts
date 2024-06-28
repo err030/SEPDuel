805ce7bcbed4b22408b9ffe66361fb7bc1985088
Beibei Tu <beibei.tu@stud.uni-due.de>
2024-06-28 17:43:13 +0200

    added components

---
+++ frontend/src/app/component/clan-list/clan-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clan-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './clan-list.component.html',
+  styleUrl: './clan-list.component.css'
+})
+export class ClanListComponent {
+
+}
