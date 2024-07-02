d9150901bd94275b1fcb9a82cfdb8e942da6f8d4
Beibei Tu <beibei.tu@stud.uni-due.de>
2024-07-02 12:32:19 +0200

    css fixed

--- frontend/src/app/component/clan-list/clan-list.component.ts
+++ frontend/src/app/component/clan-list/clan-list.component.ts
@@ -56,6 +56,7 @@ export class ClanListComponent implements OnInit {
             alert("Clan created successfully!");
             this.showCreateDialog = false;
             this.updateClanList();
+            this.goToClan();
           }
         },
         error => {
@@ -77,6 +78,7 @@ export class ClanListComponent implements OnInit {
           if (response.status === 200) {
             alert("You have joined this clan!");
             this.updateClanList();
+            this.goToClan();
           }
         }
       );
